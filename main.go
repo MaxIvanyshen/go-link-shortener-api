5c4e148eb194a5aa085467884cc593f16b7180e2
MaxIvanyshen <maxivanyshen@gmail.com>
2024-07-05 23:00:19 +0300

    feat: backup

--- main.go
+++ main.go
@@ -1,62 +1,61 @@
 package main
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"link-shortener/dto"
 	"link-shortener/link"
 	"net/http"
 	"os"
 	"time"
+    "strconv"
 
+    "github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func createLink(w http.ResponseWriter, r *http.Request) {
+func createLink(c *gin.Context) {
     var req dto.LinkReq
 
-    err := json.NewDecoder(r.Body).Decode(&req)
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-    }
+    c.BindJSON(&req)
 
-    err = db.Where("short = ?", req.Custom).First(&link.Link{}).Error
+    err := db.Where("short = ?", req.Custom).First(&link.Link{}).Error
     if err != gorm.ErrRecordNotFound {
-        w.WriteHeader(http.StatusBadRequest)
-        fmt.Fprintf(w, "custom link name taken")
+        c.Status(http.StatusConflict)
+        c.Writer.Write([]byte("custom link name taken"))
         return
     }
 
     link := link.FromDto(req)
     err = db.Create(link).Error
     if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
+        c.Status(http.StatusInternalServerError)
         return
     }
 
-    jsonData, err := json.Marshal(link)
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-    }
-
-    w.Header().Add("Content-Type", "application/json")
-    w.Write(jsonData)
+    c.IndentedJSON(http.StatusOK, link)
 }
 
-func getLink(w http.ResponseWriter, r *http.Request) {
+func getAllLinks(c *gin.Context) {
+    var allLinks []link.Link
+    db.Where("").Scopes(Paginate(c.Request)).Find(&allLinks)
+    c.IndentedJSON(http.StatusOK, allLinks)
+}
+
+func getLink(c *gin.Context) {
     var found link.Link
 
-    err := db.Where("short = ?", r.PathValue("short")).First(&found).Error
+    err := db.Where("short = ?", c.Param("short")).First(&found).Error
     if err != nil {
-        w.WriteHeader(http.StatusNotFound)
+        c.Status(http.StatusNotFound)
+        return
     }
 
     if !found.Infinite {
-        if (found.ExpiresAt != 0 && found.ExpiresAt < time.Now().Unix()) || found.Usages <= 0{
-            w.WriteHeader(http.StatusNotFound)
+        if (found.ExpiresAt != 0 && found.ExpiresAt < time.Now().Unix()) || found.Usages <= 0 {
+            c.Status(http.StatusNotFound)
             return
         } 
         if found.Usages > 0 {
@@ -67,10 +66,33 @@ func getLink(w http.ResponseWriter, r *http.Request) {
     err = db.Save(&found).Error
     if err != nil {
         fmt.Println("Error while updating link")
-        w.WriteHeader(http.StatusInternalServerError)
+        c.Status(http.StatusInternalServerError)
+        return
     }
 
-    http.Redirect(w, r, found.Original, http.StatusSeeOther)
+    c.Redirect(http.StatusMovedPermanently, found.Original)
+}
+
+
+func Paginate(r *http.Request) func(db *gorm.DB) *gorm.DB {
+  return func (db *gorm.DB) *gorm.DB {
+    q := r.URL.Query()
+    page, _ := strconv.Atoi(q.Get("page"))
+    if page <= 0 {
+      page = 1
+    }
+
+    pageSize, _ := strconv.Atoi(q.Get("page_size"))
+    switch {
+    case pageSize > 100:
+      pageSize = 100
+    case pageSize <= 0:
+      pageSize = 10
+    }
+
+    offset := (page - 1) * pageSize
+    return db.Offset(offset).Limit(pageSize)
+  }
 }
 
 var db *gorm.DB
@@ -88,7 +110,11 @@ func main() {
 
     db.AutoMigrate(&link.Link{})
 
-    http.HandleFunc("POST /api/link", createLink)
-    http.HandleFunc("GET /{short}", getLink)
-    http.ListenAndServe(":42069", nil)
+    router := gin.Default();
+
+    router.POST("/api/link", createLink)
+    router.GET("/api/link", getAllLinks)
+    router.GET("/:short", getLink)
+
+    router.Run("localhost:42069")
 }
